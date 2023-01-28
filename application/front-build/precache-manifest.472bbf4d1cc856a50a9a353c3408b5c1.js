a16a831da47edb2924fbb516fbd6adb00edd094e
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2023-01-28 00:56:54 -0300

    chore: add front-serverest build files

---
+++ application/front-build/precache-manifest.472bbf4d1cc856a50a9a353c3408b5c1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88aa23b5fd2005583fc3d1df2ebf7c3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8f9b16302c3a5577667a",
+    "url": "/static/css/2.0012cb62.chunk.css"
+  },
+  {
+    "revision": "5c611861ccfc57bf344f",
+    "url": "/static/css/main.47264059.chunk.css"
+  },
+  {
+    "revision": "8f9b16302c3a5577667a",
+    "url": "/static/js/2.186d7cf0.chunk.js"
+  },
+  {
+    "revision": "1bde7cbddc37293b60694441b834365b",
+    "url": "/static/js/2.186d7cf0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c611861ccfc57bf344f",
+    "url": "/static/js/main.554ae4f5.chunk.js"
+  },
+  {
+    "revision": "0ccef89b18fb0f56fcda",
+    "url": "/static/js/runtime-main.eb6b3d50.js"
+  },
+  {
+    "revision": "532833bab5c10a5de60a6dc3db88ddb1",
+    "url": "/static/media/serverestlogo1.532833ba.png"
+  },
+  {
+    "revision": "7e020988ad25abee813d0c6a56a66723",
+    "url": "/static/media/serverestlogo1branco.7e020988.png"
+  },
+  {
+    "revision": "f431ee71be27736de068da4c0423b587",
+    "url": "/static/media/trabalho.f431ee71.png"
+  }
+]);
