05f026db9afad313a419390b233f230f50f2b175
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2023-01-28 01:10:08 -0300

    test: use baseURL fixture

--- playwright/tests/example.spec.ts
+++ playwright/tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('http://localhost:3000');
+test('has title', async ({ page, baseURL }) => {
+  await page.goto(`${baseURL}`);
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Front - ServeRest/);
