78da8bf486cc18ed68f4376eb553757c71b74838
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2023-02-03 01:13:17 -0300

    feat(selenium): add login lean page object

---
+++ selenium/src/lean_pom/login_pom/login_pom.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass
+
+from custom_selenium import Locator
+
+from selenium.webdriver.common.by import By
+
+
+@dataclass(frozen=True, init=False)
+class LoginPOM:
+    email: Locator = (By.CSS_SELECTOR, "[data-testid='email']")
+    password: Locator = (By.CSS_SELECTOR, "[data-testid='senha']")
+    login: Locator = (By.CSS_SELECTOR, "[data-testid='entrar']")
+    register: Locator = (By.CSS_SELECTOR, "[data-testid='cadastrar']")
