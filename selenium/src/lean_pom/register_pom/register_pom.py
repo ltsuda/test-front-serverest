a0bc2b2d94f0641f2df615c57c2c654cf6aeeebd
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2023-02-03 23:48:54 -0300

    feat(selenium): add Register POM and Actions

---
+++ selenium/src/lean_pom/register_pom/register_pom.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+from custom_selenium import Locator
+
+from selenium.webdriver.common.by import By
+
+
+@dataclass(frozen=True, init=False)
+class RegisterPOM:
+    name: Locator = (By.CSS_SELECTOR, "[data-testid='nome']")
+    email: Locator = (By.CSS_SELECTOR, "[data-testid='email']")
+    password: Locator = (By.CSS_SELECTOR, "[data-testid='password']")
+    login: Locator = (By.CSS_SELECTOR, "[data-testid='entrar']")
+    register: Locator = (By.CSS_SELECTOR, "[data-testid='cadastrar']")
+    is_admin: Locator = (By.CSS_SELECTOR, "[data-testid='checkbox'][name='administrador']")
