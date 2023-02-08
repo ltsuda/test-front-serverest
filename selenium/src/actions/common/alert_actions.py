c2aae922643cc3f1dc1de4fbae689c43e697bbe8
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2023-02-08 15:33:52 -0300

    feat(selenium): refactor page actions to use custom is_displayed

--- selenium/src/actions/common/alert_actions.py
+++ selenium/src/actions/common/alert_actions.py
@@ -4,7 +4,6 @@ from custom_selenium import CustomSelenium
 from lean_pom.common.alert_pom import AlertPOM
 
 from selenium.webdriver.remote.webdriver import WebDriver
-from selenium.webdriver.remote.webelement import WebElement
 
 
 @dataclass(kw_only=True)
@@ -15,14 +14,10 @@ class AlertActions:
     def __post_init__(self):
         self.custom_selenium: CustomSelenium = CustomSelenium(self.driver)
 
-    def _alert(self, with_text: str) -> WebElement:
-        return self.custom_selenium.find_element(AlertPOM.alert_locator(with_text))
-
-    def _close_button(self, alert_text: str) -> WebElement:
-        return self.custom_selenium.find_element(AlertPOM.close_button_locator(alert_text))
-
     def is_alert_visible(self, with_text: str) -> bool:
-        return self._alert(with_text).is_displayed()
+        return self.custom_selenium.is_displayed(
+            AlertPOM.alert_locator(with_text), should_wait=False
+        )
 
     def close_alert(self, with_text: str):
-        self._close_button(with_text).click()
+        self.custom_selenium.find_element(AlertPOM.close_button_locator(with_text)).click()
--- selenium/src/custom_selenium.py
+++ selenium/src/custom_selenium.py
@@ -2,6 +2,7 @@ from contextlib import contextmanager
 from typing import Generator, LiteralString
 
 import selenium.webdriver.support.expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.support import wait
@@ -48,7 +49,7 @@ class CustomSelenium:
         yield
         set_explicit_wait(self._explicit_wait_timeout)
 
-    def find_element(self, locator: Locator, should_wait: bool = True) -> WebElement:
+    def find_element(self, locator: Locator, *, should_wait: bool = True) -> WebElement:
         """Find Selenium Webelement with explicit wait
 
         Args:
@@ -62,3 +63,26 @@ class CustomSelenium:
             return wait.WebDriverWait(self._driver, self._explicit_wait_timeout).until(
                 EC.visibility_of_element_located(locator)
             )
+
+    def is_displayed(self, locator: Locator, *, should_wait: bool = True) -> bool:  # type: ignore
+        """Check if element is displayed
+
+        It will try to wait to element to be present but not visible, then uses Selenium's function
+        to check if it's displayed. If element is not present, returns False.
+
+        Args:
+            locator (Locator): Selenium locator tuple (By.identifier, selector)
+            should_wait (bool, optional): Whether to use wait until is visible. Defaults to True.
+
+        Returns:
+            bool: if element is displayed or not
+        """
+        with self.toggle_explicit_timeout(should_wait):
+            try:
+                element: WebElement = wait.WebDriverWait(
+                    self._driver, self._explicit_wait_timeout
+                ).until(EC.presence_of_element_located(locator))
+            except TimeoutException:
+                return False
+            else:
+                element.is_displayed()
