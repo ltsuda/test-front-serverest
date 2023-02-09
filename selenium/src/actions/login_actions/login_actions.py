e6920298a9e37d5ff8ac8a9a1a67e12ed6667160
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2023-02-09 15:30:01 -0300

    chore(selenium): remove allure reporter and add pytest pretty

--- selenium/src/actions/login_actions/login_actions.py
+++ selenium/src/actions/login_actions/login_actions.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass, field
 
-import allure
 from custom_selenium import CustomSelenium
 from lean_pom.login_pom.login_pom import LoginPOM
 
@@ -15,18 +14,14 @@ class LoginActions:
     def __post_init__(self):
         self.custom_selenium: CustomSelenium = CustomSelenium(self.driver)
 
-    @allure.step
     def fill_email(self, email: str):
         self.custom_selenium.find_element(LoginPOM.email).send_keys(email)
 
-    @allure.step
     def fill_password(self, password: str):
         self.custom_selenium.find_element(LoginPOM.password).send_keys(password)
 
-    @allure.step
     def click_login(self):
         self.custom_selenium.find_element(LoginPOM.login).click()
 
-    @allure.step
     def click_register(self):
         self.custom_selenium.find_element(LoginPOM.register).click()
--- selenium/src/actions/register_actions/register_actions.py
+++ selenium/src/actions/register_actions/register_actions.py
@@ -1,6 +1,5 @@
 from dataclasses import dataclass, field
 
-import allure
 from custom_selenium import CustomSelenium
 from lean_pom.register_pom.register_pom import RegisterPOM
 
@@ -15,39 +14,31 @@ class RegisterActions:
     def __post_init__(self):
         self.custom_selenium: CustomSelenium = CustomSelenium(self.driver)
 
-    @allure.step
     def fill_name(self, name: str):
         self.custom_selenium.find_element(RegisterPOM.name).send_keys(name)
 
-    @allure.step
     def fill_email(self, email: str):
         self.custom_selenium.find_element(RegisterPOM.email).send_keys(email)
 
-    @allure.step
     def fill_password(self, password: str):
         self.custom_selenium.find_element(RegisterPOM.password).send_keys(password)
 
-    @allure.step
     def click_login(self):
         self.custom_selenium.find_element(RegisterPOM.login).click()
 
-    @allure.step
     def click_register(self):
         self.custom_selenium.find_element(RegisterPOM.register).click()
 
-    @allure.step
     def select_as_administrator(self):
         element = self.custom_selenium.find_element(RegisterPOM.is_admin)
         if not element.is_selected():
             element.click()
 
-    @allure.step
     def unselect_as_administrator(self):
         element = self.custom_selenium.find_element(RegisterPOM.is_admin)
         if element.is_selected():
             element.click()
 
-    @allure.step
     def is_register_as_administrator_selected(self) -> bool:
         return self.custom_selenium.find_element(
             RegisterPOM.is_admin, should_wait=False
