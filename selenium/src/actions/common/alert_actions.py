9f13288b74a186feb83949216a88db0e6dc3ccc2
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2023-02-08 21:46:28 -0300

    feat(selenium): add allure steps into page actions

--- selenium/src/actions/common/alert_actions.py
+++ selenium/src/actions/common/alert_actions.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass, field
 
+import allure
 from custom_selenium import CustomSelenium
 from lean_pom.common.alert_pom import AlertPOM
 
@@ -14,10 +15,12 @@ class AlertActions:
     def __post_init__(self):
         self.custom_selenium: CustomSelenium = CustomSelenium(self.driver)
 
+    @allure.step
     def is_alert_visible(self, with_text: str) -> bool:
         return self.custom_selenium.is_displayed(
             AlertPOM.alert_locator(with_text), should_wait=False
         )
 
+    @allure.step
     def close_alert(self, with_text: str):
         self.custom_selenium.find_element(AlertPOM.close_button_locator(with_text)).click()
