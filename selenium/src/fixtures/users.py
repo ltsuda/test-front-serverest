c2a9ae88446930238153c42a137235f188f05d53
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2023-02-15 10:09:11 -0300

    feat(selenium): add user and admin related fixtures

---
+++ selenium/src/fixtures/users.py
@@ -0,0 +1,24 @@
+import pytest
+from src.fixtures.api import create_account
+from src.keywords.user_data import UserData
+from src.model.user import UserWithIDProtocol
+
+
+@pytest.fixture(scope="function")
+def regular_user() -> UserData:
+    return UserData(as_admin=False)
+
+
+@pytest.fixture(scope="function")
+def admin_user() -> UserData:
+    return UserData(as_admin=True)
+
+
+@pytest.fixture(scope="function")
+def user_account(regular_user, create_account) -> UserWithIDProtocol:
+    return create_account(regular_user.data())
+
+
+@pytest.fixture(scope="function")
+def admin_account(admin_user, create_account) -> UserWithIDProtocol:
+    return create_account(admin_user.data())
