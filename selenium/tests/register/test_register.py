84d403dab5bd46cd4ee4055c280782cdbbf5733f
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2023-02-21 16:40:46 -0300

    test(selenium): implement registration test cases

---
+++ selenium/tests/register/test_register.py
@@ -0,0 +1,110 @@
+import pytest
+import src.pom.actions.register_actions.register as register_actions
+import src.pom.query.admin_home.admin_home as admin_home
+import src.pom.query.common.alert as alert_query
+import src.pom.query.register.register as register_query
+import src.pom.query.user_home.user_home as user_home
+from src.consts import URL
+
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+@pytest.mark.register
+class TestLogin:
+    @pytest.mark.register_001
+    def test_admin_register_redirected_to_home(self, driver: WebDriver, admin_user, base_url):
+        driver.get(f"{base_url}{URL.register}")
+        admin_data = admin_user.data()
+        _register_action = register_actions.RegisterActions(driver=driver)
+        _register_query = register_query.RegisterQuery(driver=driver)
+        _alert_query = alert_query.AlertQuery(driver=driver)
+        _admin_home = admin_home.AdminHomeQuery(driver=driver)
+
+        _register_action.fill_name(admin_data.name)
+        _register_action.fill_email(admin_data.email)
+        _register_action.fill_password(admin_data.password)
+        _register_action.select_as_administrator()
+        assert _register_query.is_register_as_administrator_selected()
+        _register_action.click_register()
+
+        assert _alert_query.is_alert_visible(with_text="Cadastro realizado com sucesso")
+        assert _admin_home.has_welcome_message(for_user=admin_data.name)
+        assert _admin_home.is_store_description_visible()
+        assert "admin" in driver.current_url
+
+    @pytest.mark.register_002
+    def test_user__register_redirected_to_home(self, driver: WebDriver, regular_user, base_url):
+        driver.get(f"{base_url}{URL.register}")
+        user_data = regular_user.data()
+        _register_action = register_actions.RegisterActions(driver=driver)
+        _alert_query = alert_query.AlertQuery(driver=driver)
+        _register_query = register_query.RegisterQuery(driver=driver)
+        _user_home = user_home.UserHomeQuery(driver=driver)
+
+        _register_action.fill_name(user_data.name)
+        _register_action.fill_email(user_data.email)
+        _register_action.fill_password(user_data.password)
+        assert _register_query.is_register_as_administrator_selected() is False
+        _register_action.click_register()
+
+        assert _alert_query.is_alert_visible(with_text="Cadastro realizado com sucesso")
+        assert _user_home.is_store_visible()
+        assert "admin" not in driver.current_url
+
+    @pytest.mark.register_003
+    def test_redirected_to_login(self, driver: WebDriver, base_url):
+        driver.get(f"{base_url}{URL.register}")
+        _register_actions = register_actions.RegisterActions(driver=driver)
+
+        _register_actions.click_login()
+
+        assert driver.current_url == f"{base_url}{URL.login}"
+
+    @pytest.mark.register_004
+    def test_alert_register_name_is_required(self, driver: WebDriver, base_url):
+        driver.get(f"{base_url}{URL.register}")
+        _register_actions = register_actions.RegisterActions(driver=driver)
+        _alert_query = alert_query.AlertQuery(driver=driver)
+
+        _register_actions.fill_email("user@provider.com")
+        _register_actions.fill_password("password")
+        _register_actions.click_register()
+
+        assert _alert_query.is_alert_visible(with_text="Nome é obrigatório")
+
+    @pytest.mark.register_005
+    def test_alert_register_email_is_required(self, driver: WebDriver, base_url):
+        driver.get(f"{base_url}{URL.register}")
+        _register_actions = register_actions.RegisterActions(driver=driver)
+        _alert_query = alert_query.AlertQuery(driver=driver)
+
+        _register_actions.fill_name("John Doe")
+        _register_actions.fill_password("password")
+        _register_actions.click_register()
+
+        assert _alert_query.is_alert_visible(with_text="Email é obrigatório")
+
+    @pytest.mark.register_006
+    def test_alert_register_password_is_required(self, driver: WebDriver, base_url):
+        driver.get(f"{base_url}{URL.register}")
+        _register_actions = register_actions.RegisterActions(driver=driver)
+        _alert_query = alert_query.AlertQuery(driver=driver)
+
+        _register_actions.fill_name("John Doe")
+        _register_actions.fill_email("user@provider.com")
+        _register_actions.click_register()
+
+        assert _alert_query.is_alert_visible(with_text="Password é obrigatório")
+
+    @pytest.mark.register_007
+    def test_alert_email_already_in_use(self, driver: WebDriver, user_account, base_url):
+        driver.get(f"{base_url}{URL.register}")
+        _register_actions = register_actions.RegisterActions(driver=driver)
+        _alert_query = alert_query.AlertQuery(driver=driver)
+
+        _register_actions.fill_name("John Doe")
+        _register_actions.fill_email(user_account.email)
+        _register_actions.fill_password("password")
+        _register_actions.click_register()
+
+        assert _alert_query.is_alert_visible(with_text="Este email já está sendo usado")
