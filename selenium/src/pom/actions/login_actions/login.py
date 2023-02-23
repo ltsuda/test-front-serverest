8e5b104d51bbe054927ddb36c8202376d300cc6e
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2023-02-23 16:03:09 -0300

    chore(selenium): move alert pom into pages and refactor tests

--- selenium/src/pom/actions/login_actions/login.py
+++ selenium/src/pom/actions/login_actions/login.py
@@ -1,18 +1,22 @@
 from dataclasses import dataclass, field
 
-from custom_selenium import CustomSelenium
-from selenium.webdriver.remote.webdriver import WebDriver
 from src.consts import URL
+from src.custom_selenium import CustomSelenium
+from src.pom.actions.common.alert import AlertActions
 from src.pom.ui.login.login import LoginUI
+
+from selenium.webdriver.remote.webdriver import WebDriver
 
 
 @dataclass(kw_only=True)
 class LoginActions:
     driver: WebDriver
     custom_selenium: CustomSelenium = field(init=False)
+    alert: AlertActions = field(init=False)
 
     def __post_init__(self):
         self.custom_selenium: CustomSelenium = CustomSelenium(self.driver)
+        self.alert: AlertActions = AlertActions(driver=self.driver)
 
     def goto(self) -> str:
         login_url = f"{URL.base_url}{URL.login}"
--- selenium/tests/login/test_login.py
+++ selenium/tests/login/test_login.py
@@ -1,11 +1,11 @@
 import uuid
 
 import pytest
-import src.pom.actions.login_actions.login as login_actions
-import src.pom.query.admin_home.admin_home as admin_home
-import src.pom.query.common.alert as alert_query
-import src.pom.query.user_home.user_home as user_home
 from src.consts import URL
+from src.pom.actions.login_actions import login as login_actions
+from src.pom.query.admin_home import admin_home as admin_home
+from src.pom.query.login import login as login_query
+from src.pom.query.user_home import user_home as user_home
 
 from selenium.webdriver.remote.webdriver import WebDriver
 
@@ -20,9 +20,9 @@ class TestLogin:
         _login_action.fill_password(admin_account.password)
         _login_action.click_login()
 
-        assert "admin" in driver.current_url
         assert _admin_home.has_welcome_message(for_user=admin_account.name)
         assert _admin_home.is_store_description_visible()
+        assert "admin" in driver.current_url
 
     @pytest.mark.login_002
     def test_user_redirected_to_home(self, driver: WebDriver, user_account):
@@ -47,41 +47,41 @@ class TestLogin:
     @pytest.mark.login_004
     def test_alert_email_is_required(self, driver: WebDriver):
         _login_actions = login_actions.LoginActions(driver=driver)
-        _alert_query = alert_query.AlertQuery(driver=driver)
+        _login_query = login_query.LoginQuery(driver=driver)
 
         _login_actions.fill_password("password")
         _login_actions.click_login()
 
-        assert _alert_query.is_alert_visible(with_text="Email é obrigatório")
+        assert _login_query.alert.is_alert_visible(with_text="Email é obrigatório")
 
     @pytest.mark.login_005
     def test_alert_password_is_required(self, driver: WebDriver):
         _login_actions = login_actions.LoginActions(driver=driver)
-        _alert_query = alert_query.AlertQuery(driver=driver)
+        _login_query = login_query.LoginQuery(driver=driver)
 
         _login_actions.fill_email("user@provider.com")
         _login_actions.click_login()
 
-        assert _alert_query.is_alert_visible(with_text="Password é obrigatório")
+        assert _login_query.alert.is_alert_visible(with_text="Password é obrigatório")
 
     @pytest.mark.login_006
     def test_alert_email_not_registered(self, driver: WebDriver):
         _login_actions = login_actions.LoginActions(driver=driver)
-        _alert_query = alert_query.AlertQuery(driver=driver)
+        _login_query = login_query.LoginQuery(driver=driver)
 
         _login_actions.fill_email(f"{uuid.uuid4()}@provider.com")
         _login_actions.fill_password("password")
         _login_actions.click_login()
 
-        assert _alert_query.is_alert_visible(with_text="Email e/ou senha inválidos")
+        assert _login_query.alert.is_alert_visible(with_text="Email e/ou senha inválidos")
 
     @pytest.mark.login_007
     def test_alert_invalid_credentials(self, driver: WebDriver, user_account):
         _login_actions = login_actions.LoginActions(driver=driver)
-        _alert_query = alert_query.AlertQuery(driver=driver)
+        _login_query = login_query.LoginQuery(driver=driver)
 
         _login_actions.fill_email(user_account.email)
         _login_actions.fill_password(f"{uuid.uuid4()}")
         _login_actions.click_login()
 
-        assert _alert_query.is_alert_visible(with_text="Email e/ou senha inválidos")
+        assert _login_query.alert.is_alert_visible(with_text="Email e/ou senha inválidos")
