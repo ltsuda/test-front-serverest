000856a389e76be26293ab10cf819f9710af152b
Leonardo Tsuda <leonardo.tsuda@icloud.com>
2023-02-28 13:41:01 -0300

    chore(selenium): refactor navigator and test cases to use factories

--- selenium/tests/login/test_login.py
+++ selenium/tests/login/test_login.py
@@ -1,11 +1,15 @@
+import typing
 import uuid
 
 import pytest
-from src.consts import URL
-from src.pom.actions.login_actions import login as login_actions
-from src.pom.query.admin_home import admin_home as admin_home
-from src.pom.query.login import login as login_query
-from src.pom.query.user_home import user_home as user_home
+from src import factory
+from src.consts import URL, Pages
+
+if typing.TYPE_CHECKING:
+    from src.pom.actions.login_actions.login import LoginActions
+    from src.pom.query.admin_home.admin_home import AdminHomeQuery
+    from src.pom.query.login.login import LoginQuery
+    from src.pom.query.user_home.user_home import UserHomeQuery
 
 from selenium.webdriver.remote.webdriver import WebDriver
 
@@ -14,11 +18,15 @@ from selenium.webdriver.remote.webdriver import WebDriver
 class TestLogin:
     @pytest.mark.login_001
     def test_admin_redirected_to_home(self, driver: WebDriver, admin_account):
-        _login_action = login_actions.LoginActions(driver=driver)
-        _admin_home = admin_home.AdminHomeQuery(driver=driver)
-        _login_action.fill_email(admin_account.email)
-        _login_action.fill_password(admin_account.password)
-        _login_action.click_login()
+        _login_actions: LoginActions = factory.ActionsFactory.get(
+            Pages.LOGIN, driver=driver  # type: ignore
+        )
+        _admin_home: AdminHomeQuery = factory.QueriesFactory.get(
+            Pages.ADMIN_HOME, driver=driver  # type: ignore
+        )
+        _login_actions.fill_email(admin_account.email)
+        _login_actions.fill_password(admin_account.password)
+        _login_actions.click_login()
 
         assert _admin_home.has_welcome_message(for_user=admin_account.name)
         assert _admin_home.is_store_description_visible()
@@ -26,19 +34,25 @@ class TestLogin:
 
     @pytest.mark.login_002
     def test_user_redirected_to_home(self, driver: WebDriver, user_account):
-        _login_action = login_actions.LoginActions(driver=driver)
-        _user_home = user_home.UserHomeQuery(driver=driver)
+        _login_actions: LoginActions = factory.ActionsFactory.get(
+            Pages.LOGIN, driver=driver  # type: ignore
+        )
+        _user_home: UserHomeQuery = factory.QueriesFactory.get(
+            Pages.USER_HOME, driver=driver  # type: ignore
+        )
 
-        _login_action.fill_email(user_account.email)
-        _login_action.fill_password(user_account.password)
-        _login_action.click_login()
+        _login_actions.fill_email(user_account.email)
+        _login_actions.fill_password(user_account.password)
+        _login_actions.click_login()
 
         assert "admin" not in driver.current_url
         assert _user_home.is_store_visible()
 
     @pytest.mark.login_003
     def test_redirected_to_register(self, driver: WebDriver):
-        _login_actions = login_actions.LoginActions(driver=driver)
+        _login_actions: LoginActions = factory.ActionsFactory.get(
+            Pages.LOGIN, driver=driver  # type: ignore
+        )
 
         _login_actions.click_register()
 
@@ -46,8 +60,12 @@ class TestLogin:
 
     @pytest.mark.login_004
     def test_alert_email_is_required(self, driver: WebDriver):
-        _login_actions = login_actions.LoginActions(driver=driver)
-        _login_query = login_query.LoginQuery(driver=driver)
+        _login_actions: LoginActions = factory.ActionsFactory.get(
+            Pages.LOGIN, driver=driver  # type: ignore
+        )
+        _login_query: LoginQuery = factory.QueriesFactory.get(
+            Pages.LOGIN, driver=driver  # type: ignore
+        )
 
         _login_actions.fill_password("password")
         _login_actions.click_login()
@@ -56,8 +74,12 @@ class TestLogin:
 
     @pytest.mark.login_005
     def test_alert_password_is_required(self, driver: WebDriver):
-        _login_actions = login_actions.LoginActions(driver=driver)
-        _login_query = login_query.LoginQuery(driver=driver)
+        _login_actions: LoginActions = factory.ActionsFactory.get(
+            Pages.LOGIN, driver=driver  # type: ignore
+        )
+        _login_query: LoginQuery = factory.QueriesFactory.get(
+            Pages.LOGIN, driver=driver  # type: ignore
+        )
 
         _login_actions.fill_email("user@provider.com")
         _login_actions.click_login()
@@ -66,8 +88,12 @@ class TestLogin:
 
     @pytest.mark.login_006
     def test_alert_email_not_registered(self, driver: WebDriver):
-        _login_actions = login_actions.LoginActions(driver=driver)
-        _login_query = login_query.LoginQuery(driver=driver)
+        _login_actions: LoginActions = factory.ActionsFactory.get(
+            Pages.LOGIN, driver=driver  # type: ignore
+        )
+        _login_query: LoginQuery = factory.QueriesFactory.get(
+            Pages.LOGIN, driver=driver  # type: ignore
+        )
 
         _login_actions.fill_email(f"{uuid.uuid4()}@provider.com")
         _login_actions.fill_password("password")
@@ -77,8 +103,12 @@ class TestLogin:
 
     @pytest.mark.login_007
     def test_alert_invalid_credentials(self, driver: WebDriver, user_account):
-        _login_actions = login_actions.LoginActions(driver=driver)
-        _login_query = login_query.LoginQuery(driver=driver)
+        _login_actions: LoginActions = factory.ActionsFactory.get(
+            Pages.LOGIN, driver=driver  # type: ignore
+        )
+        _login_query: LoginQuery = factory.QueriesFactory.get(
+            Pages.LOGIN, driver=driver  # type: ignore
+        )
 
         _login_actions.fill_email(user_account.email)
         _login_actions.fill_password(f"{uuid.uuid4()}")
--- selenium/tests/register/test_register.py
+++ selenium/tests/register/test_register.py
@@ -1,31 +1,42 @@
+import typing
+
 import pytest
-import src.pom.actions.register_actions.register as register_actions
-import src.pom.query.admin_home.admin_home as admin_home
-import src.pom.query.register.register as register_query
-import src.pom.query.user_home.user_home as user_home
-from src.consts import URL
+from src import factory
+from src.consts import URL, Pages
 from src.keywords.navigation import Navigator
+
+if typing.TYPE_CHECKING:
+    from src.pom.actions.register_actions.register import RegisterActions
+    from src.pom.query.admin_home.admin_home import AdminHomeQuery
+    from src.pom.query.register.register import RegisterQuery
+    from src.pom.query.user_home.user_home import UserHomeQuery
 
 from selenium.webdriver.remote.webdriver import WebDriver
 
 
 @pytest.mark.register
-class TestLogin:
+class TestRegister:
     @pytest.mark.register_001
     def test_admin_register_redirected_to_home(self, driver: WebDriver, admin_user):
-        _register_action = register_actions.RegisterActions(driver=driver)
-        _register_query = register_query.RegisterQuery(driver=driver)
-        _admin_home = admin_home.AdminHomeQuery(driver=driver)
+        _register_actions: RegisterActions = factory.ActionsFactory.get(
+            Pages.REGISTER, driver  # type: ignore
+        )
+        _register_query: RegisterQuery = factory.QueriesFactory.get(
+            Pages.REGISTER, driver  # type: ignore
+        )
+        _admin_home: AdminHomeQuery = factory.QueriesFactory.get(
+            Pages.ADMIN_HOME, driver=driver  # type: ignore
+        )
         admin_data = admin_user.data()
         navigator = Navigator(driver=driver)
         navigator.navigate_to_register()
 
-        _register_action.fill_name(admin_data.name)
-        _register_action.fill_email(admin_data.email)
-        _register_action.fill_password(admin_data.password)
-        _register_action.select_as_administrator()
+        _register_actions.fill_name(admin_data.name)
+        _register_actions.fill_email(admin_data.email)
+        _register_actions.fill_password(admin_data.password)
+        _register_actions.select_as_administrator()
         assert _register_query.is_register_as_administrator_selected()
-        _register_action.click_register()
+        _register_actions.click_register()
 
         assert _register_query.alert.is_alert_visible(with_text="Cadastro realizado com sucesso")
         assert _admin_home.has_welcome_message(for_user=admin_data.name)
@@ -34,18 +45,24 @@ class TestLogin:
 
     @pytest.mark.register_002
     def test_user__register_redirected_to_home(self, driver: WebDriver, regular_user):
-        _register_action = register_actions.RegisterActions(driver=driver)
-        _register_query = register_query.RegisterQuery(driver=driver)
-        _user_home = user_home.UserHomeQuery(driver=driver)
+        _register_actions: RegisterActions = factory.ActionsFactory.get(
+            Pages.REGISTER, driver  # type: ignore
+        )
+        _register_query: RegisterQuery = factory.QueriesFactory.get(
+            Pages.REGISTER, driver  # type: ignore
+        )
+        _user_home: UserHomeQuery = factory.QueriesFactory.get(
+            Pages.USER_HOME, driver=driver  # type: ignore
+        )
         user_data = regular_user.data()
         navigator = Navigator(driver=driver)
         navigator.navigate_to_register()
 
-        _register_action.fill_name(user_data.name)
-        _register_action.fill_email(user_data.email)
-        _register_action.fill_password(user_data.password)
+        _register_actions.fill_name(user_data.name)
+        _register_actions.fill_email(user_data.email)
+        _register_actions.fill_password(user_data.password)
         assert _register_query.is_register_as_administrator_selected() is False
-        _register_action.click_register()
+        _register_actions.click_register()
 
         assert _register_query.alert.is_alert_visible(with_text="Cadastro realizado com sucesso")
         assert _user_home.is_store_visible()
@@ -53,7 +70,9 @@ class TestLogin:
 
     @pytest.mark.register_003
     def test_redirected_to_login(self, driver: WebDriver):
-        _register_actions = register_actions.RegisterActions(driver=driver)
+        _register_actions: RegisterActions = factory.ActionsFactory.get(
+            Pages.REGISTER, driver  # type: ignore
+        )
         navigator = Navigator(driver=driver)
         navigator.navigate_to_register()
 
@@ -63,8 +82,12 @@ class TestLogin:
 
     @pytest.mark.register_004
     def test_alert_register_name_is_required(self, driver: WebDriver):
-        _register_actions = register_actions.RegisterActions(driver=driver)
-        _register_query = register_query.RegisterQuery(driver=driver)
+        _register_actions: RegisterActions = factory.ActionsFactory.get(
+            Pages.REGISTER, driver  # type: ignore
+        )
+        _register_query: RegisterQuery = factory.QueriesFactory.get(
+            Pages.REGISTER, driver  # type: ignore
+        )
         navigator = Navigator(driver=driver)
         navigator.navigate_to_register()
 
@@ -76,8 +99,12 @@ class TestLogin:
 
     @pytest.mark.register_005
     def test_alert_register_email_is_required(self, driver: WebDriver):
-        _register_actions = register_actions.RegisterActions(driver=driver)
-        _register_query = register_query.RegisterQuery(driver=driver)
+        _register_actions: RegisterActions = factory.ActionsFactory.get(
+            Pages.REGISTER, driver  # type: ignore
+        )
+        _register_query: RegisterQuery = factory.QueriesFactory.get(
+            Pages.REGISTER, driver  # type: ignore
+        )
         navigator = Navigator(driver=driver)
         navigator.navigate_to_register()
 
@@ -89,8 +116,12 @@ class TestLogin:
 
     @pytest.mark.register_006
     def test_alert_register_password_is_required(self, driver: WebDriver):
-        _register_actions = register_actions.RegisterActions(driver=driver)
-        _register_query = register_query.RegisterQuery(driver=driver)
+        _register_actions: RegisterActions = factory.ActionsFactory.get(
+            Pages.REGISTER, driver  # type: ignore
+        )
+        _register_query: RegisterQuery = factory.QueriesFactory.get(
+            Pages.REGISTER, driver  # type: ignore
+        )
         navigator = Navigator(driver=driver)
         navigator.navigate_to_register()
 
@@ -102,8 +133,12 @@ class TestLogin:
 
     @pytest.mark.register_007
     def test_alert_email_already_in_use(self, driver: WebDriver, user_account):
-        _register_actions = register_actions.RegisterActions(driver=driver)
-        _register_query = register_query.RegisterQuery(driver=driver)
+        _register_actions: RegisterActions = factory.ActionsFactory.get(
+            Pages.REGISTER, driver  # type: ignore
+        )
+        _register_query: RegisterQuery = factory.QueriesFactory.get(
+            Pages.REGISTER, driver  # type: ignore
+        )
         navigator = Navigator(driver=driver)
         navigator.navigate_to_register()
 
