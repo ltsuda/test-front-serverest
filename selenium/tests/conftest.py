b2e1def6b97e4e126d4c99a5f66a8b3053985da1
Leonardo Tsuda <3965277+ltsuda@users.noreply.github.com>
2023-03-21 22:41:45 -0300

    chore(selenium): add noqa to stop linter errors on fixtures

--- selenium/tests/conftest.py
+++ selenium/tests/conftest.py
@@ -1,2 +1,2 @@
-from src.fixtures.api import create_account, get_authorization_token
-from src.fixtures.users import admin_account, admin_user, regular_user, user_account
+from src.fixtures.api import create_account  # noqa
+from src.fixtures.users import admin_account, admin_user, regular_user, user_account  # noqa
